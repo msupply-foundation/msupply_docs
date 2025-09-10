4c4181eb946402b56b4f781fdc0adbea0ff0f8d2
mark-prins <mark@malcolmprins.net>
2025-09-10 16:47:52 +1200

    show type ahead search

--- themes/adidoks/static/js/search.js
+++ themes/adidoks/static/js/search.js
@@ -13,21 +13,47 @@ function inputFocus(e) {
     userinput.focus();
   }
 
-  // if (e.keyCode === 27 ) {
-  //   userinput.blur();
-  //   suggestions.classList.add('d-none');
-  // }
+  if (e.keyCode === 27) {
+    userinput.blur();
+    suggestions.classList.add('d-none');
+  }
 }
 
-// document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
+  var isClickInsideElement = suggestions.contains(event.target);
 
-//   var isClickInsideElement = suggestions.contains(event.target);
+  if (!isClickInsideElement) {
+    suggestions.classList.add('d-none');
+  }
+});
 
-//   if (!isClickInsideElement) {
-//     suggestions.classList.add('d-none');
-//   }
+/*
+Source:
+  - https://dev.to/shubhamprakash/trap-focus-using-javascript-6a3
+*/
 
-// });
+document.addEventListener('keydown', suggestionFocus);
+
+function suggestionFocus(e) {
+  const focusableSuggestions = suggestions.querySelectorAll('a');
+  if (suggestions.classList.contains('d-none') || focusableSuggestions.length === 0) {
+    return;
+  }
+  const focusable = [...focusableSuggestions];
+  const index = focusable.indexOf(document.activeElement);
+
+  let nextIndex = 0;
+
+  if (e.keyCode === 38) {
+    e.preventDefault();
+    nextIndex = index > 0 ? index - 1 : 0;
+    focusableSuggestions[nextIndex].focus();
+  } else if (e.keyCode === 40) {
+    e.preventDefault();
+    nextIndex = index + 1 < focusable.length ? index + 1 : index;
+    focusableSuggestions[nextIndex].focus();
+  }
+}
 
 /*
 Source:
@@ -36,59 +62,60 @@ Source:
   - http://elasticlunr.com/
   - https://github.com/getzola/zola/blob/master/docs/static/search.js
 */
-/*
-(function(){
+(function () {
   var index = elasticlunr.Index.load(window.searchIndex);
   userinput.addEventListener('input', show_results, true);
   suggestions.addEventListener('click', accept_suggestion, true);
-  
-  function show_results(){
+
+  function show_results() {
     var value = this.value.trim();
     var options = {
-      bool: "OR",
+      bool: 'OR',
       fields: {
-        title: {boost: 2},
-        body: {boost: 1},
-      }
+        title: { boost: 2 },
+        body: { boost: 1 },
+      },
     };
     var results = index.search(value, options);
 
-    var entry, childs = suggestions.childNodes;
-    var i = 0, len = results.length;
+    var entry,
+      childs = suggestions.childNodes;
+    var i = 0,
+      len = results.length;
     var items = value.split(/\s+/);
     suggestions.classList.remove('d-none');
 
-    results.forEach(function(page) {
+    results.forEach(function (page) {
+      // this is just to filter out any results that are not in this section
+      if (!!currentSection && !searchDocuments.find((d) => d.id === page.ref)) return;
+
       if (page.doc.body !== '') {
         entry = document.createElement('div');
 
         entry.innerHTML = '<a href><span></span><span></span></a>';
-  
-        a = entry.querySelector('a'),
-        t = entry.querySelector('span:first-child'),
-        d = entry.querySelector('span:nth-child(2)');
+
+        (a = entry.querySelector('a')),
+          (t = entry.querySelector('span:first-child')),
+          (d = entry.querySelector('span:nth-child(2)'));
         a.href = page.ref;
         t.textContent = page.doc.title;
         d.innerHTML = makeTeaser(page.doc.body, items);
-  
+
         suggestions.appendChild(entry);
       }
     });
 
-    while(childs.length > len){
-        suggestions.removeChild(childs[i])
+    while (childs.length > len) {
+      suggestions.removeChild(childs[i]);
     }
-
   }
 
-  function accept_suggestion(){
+  function accept_suggestion() {
+    while (suggestions.lastChild) {
+      suggestions.removeChild(suggestions.lastChild);
+    }
 
-      while(suggestions.lastChild){
-
-          suggestions.removeChild(suggestions.lastChild);
-      }
-
-      return false;
+    return false;
   }
 
   // Taken from mdbook
@@ -106,23 +133,22 @@ Source:
     var NORMAL_WORD_WEIGHT = 2;
     var FIRST_WORD_WEIGHT = 8;
     var TEASER_MAX_WORDS = 30;
-  
+
     var stemmedTerms = terms.map(function (w) {
       return elasticlunr.stemmer(w.toLowerCase());
     });
     var termFound = false;
     var index = 0;
     var weighted = []; // contains elements of ["word", weight, index_in_document]
-  
+
     // split in sentences, then words
-    var sentences = body.toLowerCase().split(". ");
+    var sentences = body.toLowerCase().split('. ');
     for (var i in sentences) {
       var words = sentences[i].split(/[\s\n]/);
       var value = FIRST_WORD_WEIGHT;
       for (var j in words) {
-        
         var word = words[j];
-  
+
         if (word.length > 0) {
           for (var k in stemmedTerms) {
             if (elasticlunr.stemmer(word).startsWith(stemmedTerms[k])) {
@@ -133,14 +159,14 @@ Source:
           weighted.push([word, value, index]);
           value = NORMAL_WORD_WEIGHT;
         }
-  
+
         index += word.length;
-        index += 1;  // ' ' or '.' if last word in sentence
+        index += 1; // ' ' or '.' if last word in sentence
       }
-  
-      index += 1;  // because we split at a two-char boundary '. '
+
+      index += 1; // because we split at a two-char boundary '. '
     }
-  
+
     if (weighted.length === 0) {
       if (body.length !== undefined && body.length > TEASER_MAX_WORDS * 10) {
         return body.substring(0, TEASER_MAX_WORDS * 10) + '...';
@@ -148,7 +174,7 @@ Source:
         return body;
       }
     }
-  
+
     var windowWeights = [];
     var windowSize = Math.min(weighted.length, TEASER_MAX_WORDS);
     // We add a window with all the weights first
@@ -157,13 +183,13 @@ Source:
       curSum += weighted[i][1];
     }
     windowWeights.push(curSum);
-  
+
     for (var i = 0; i < weighted.length - windowSize; i++) {
       curSum -= weighted[i][1];
       curSum += weighted[i + windowSize][1];
       windowWeights.push(curSum);
     }
-  
+
     // If we didn't find the term, just pick the first window
     var maxSumIndex = 0;
     if (termFound) {
@@ -176,7 +202,7 @@ Source:
         }
       }
     }
-  
+
     var teaser = [];
     var startIndex = weighted[maxSumIndex][2];
     for (var i = maxSumIndex; i < maxSumIndex + windowSize; i++) {
@@ -186,15 +212,15 @@ Source:
         teaser.push(body.substring(startIndex, word[2]));
         startIndex = word[2];
       }
-  
+
       // add <em/> around search terms
       if (word[1] === TERM_WEIGHT) {
-        teaser.push("<b>");
+        teaser.push('<b>');
       }
 
       startIndex = word[2] + word[0].length;
       // Check the string is ascii characters or not
-      var re = /^[\x00-\xff]+$/
+      var re = /^[\x00-\xff]+$/;
       if (word[1] !== TERM_WEIGHT && word[0].length >= 12 && !re.test(word[0])) {
         // If the string's length is too long, it maybe a Chinese/Japance/Korean article
         // if using substring method directly, it may occur error codes on emoji chars
@@ -204,22 +230,21 @@ Source:
       } else {
         teaser.push(body.substring(word[2], startIndex));
       }
-  
+
       if (word[1] === TERM_WEIGHT) {
-        teaser.push("</b>");
+        teaser.push('</b>');
       }
     }
-    teaser.push("…");
-    return teaser.join("");
+    teaser.push('…');
+    return teaser.join('');
   }
-}());
-
+})();
 
 // Get substring by bytes
-// If using JavaScript inline substring method, it will return error codes 
+// If using JavaScript inline substring method, it will return error codes
 // Source: https://www.52pojie.cn/thread-1059814-1-1.html
 function substringByByte(str, maxLength) {
-  var result = "";
+  var result = '';
   var flag = false;
   var len = 0;
   var length = 0;
@@ -228,8 +253,8 @@ function substringByByte(str, maxLength) {
     var code = str.codePointAt(i).toString(16);
     if (code.length > 4) {
       i++;
-      if ((i + 1) < str.length) {
-        flag = str.codePointAt(i + 1).toString(16) == "200d";
+      if (i + 1 < str.length) {
+        flag = str.codePointAt(i + 1).toString(16) == '200d';
       }
     }
     if (flag) {
@@ -239,7 +264,7 @@ function substringByByte(str, maxLength) {
         if (length <= maxLength) {
           result += str.substr(length2, i - length2 + 1);
         } else {
-          break
+          break;
         }
       }
     } else {
@@ -250,7 +275,7 @@ function substringByByte(str, maxLength) {
           result += str.substr(length2, i - length2 + 1);
           length2 = i + 1;
         } else {
-          break
+          break;
         }
         len = 0;
         continue;
@@ -258,13 +283,13 @@ function substringByByte(str, maxLength) {
       length += getByteByHex(code);
       if (length <= maxLength) {
         if (code.length <= 4) {
-          result += str[i]
+          result += str[i];
         } else {
-          result += str[i - 1] + str[i]
+          result += str[i - 1] + str[i];
         }
         length2 = i + 1;
       } else {
-        break
+        break;
       }
     }
   }
@@ -285,4 +310,3 @@ function getByteByBinary(binaryCode) {
 function getByteByHex(hexCode) {
   return getByteByBinary(parseInt(hexCode, 16).toString(2));
 }
-*/
